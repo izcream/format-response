671114a45021513cb130df9dd0493d118f2bf805
izcream* <natakorn@wewillapp.com>
2019-02-21 16:09:42 +0700

    add render method

--- src/FormatResponse.php
+++ src/FormatResponse.php
@@ -1,24 +1,34 @@
 <?php
 namespace Wewillapp;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+
 class FormatResponse
 {
-    public static function success($data = [], $message = null)
-    {
-        if (!is_null($message)) {
-            $data['message'] = $message;
-        }
-        return response()->json([
-            'data' => $data,
-        ], 200);
-    }
-
-    public static function error($message, $statusCode = 400)
+    private static function error($message, $statusCode = 400)
     {
         return response()->json([
             'errors' => [
                 'message' => $message,
             ],
         ], $statusCode);
+    }
+
+    public static function render($exception)
+    {
+        $statusCode = 500;
+        $message = snake_case($exception->getMessage());
+        if ($exception instanceof ModelNotFoundException) {
+            $statusCode = 404;
+        } elseif ($exception instanceof ValidationException) {
+            $statusCode = isset($exception->status) ? $exception->status : 422;
+            $message = snake_case($exception->validator->errors()->first());
+        } elseif ($exception instanceof \Illuminate\Auth\AuthenticationException) {
+            $statusCode = 401;
+        } else {
+            $statusCode = isset($exception->status) ? $exception->status : 500;
+        }
+        return self::error($message, $statusCode);
     }
 }
